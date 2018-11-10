77cf5418f5ccf345c026a44c7883c8b2d79e4106
Kirill Makarenko <makarenko.kiri@gmail.com>
2018-11-10 12:16:06 +0300

    Initial Commit

---
+++ NewsfeedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsfeedApp
+//
+//  Created by Кирилл on 10/11/2018.
+//  Copyright © 2018 Кирилл. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
